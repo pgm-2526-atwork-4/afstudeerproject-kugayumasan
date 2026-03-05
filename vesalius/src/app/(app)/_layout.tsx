734307979ba7519a5d1f3a24be5baa25781ba236
kugayumasan <erosferri@hotmail.com>
2026-03-05 18:22:08 +0100

    chore(app): defines navigation for authenticated part of the app

--- vesalius/src/app/(app)/_layout.tsx
+++ vesalius/src/app/(app)/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
+import { SessionProvider } from "@core/modules/session/session.context";
 
-export default function AppLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+export default function RootLayout() {
+  return (
+    <SessionProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(app)" />
+      </Stack>
+    </SessionProvider>
+  );
 }
