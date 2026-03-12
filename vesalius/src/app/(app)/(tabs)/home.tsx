2ef68f9db93cf187ebb3c3b540e8ca2f187e0de0
kugayumasan <erosferri@hotmail.com>
2026-03-12 18:52:26 +0100

    feat: add reusable UI elements and hooks for interaction and patient flows

--- vesalius/src/app/(app)/(tabs)/home.tsx
+++ vesalius/src/app/(app)/(tabs)/home.tsx
@@ -1,18 +1,5 @@
-import { router } from "expo-router";
-import HomeScreen from "@design/screens/HomeScreen";
+import HomeContainer from "@functional/home/HomeContainer";
 
 export default function Home() {
-  return (
-    <HomeScreen
-      onNewInteraction={() => {
-        router.push("/(app)/(tabs)/record");
-      }}
-      onViewAllInteractions={() => {
-        router.push("/(app)/(tabs)/interactions");
-      }}
-      onViewInteraction={(id) => {
-        router.push(`/(app)/interactions/${id}`);
-      }}
-    />
-  );
+  return <HomeContainer />;
 }
